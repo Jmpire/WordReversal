9819a1d872fde375abf236c1d4310d28ed2a6724
Malakia Jeremia <malakiajerr2@gmail.com>
2022-11-29 18:51:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Abstraction3
+{
+    internal class Program
+    {
+        static bool Reverse(string msg)
+        {
+            bool word = false;
+            // Remove all empty spaces
+            string spaceless = msg.Replace(" ", "");
+            // Turn entire sentence to lowercase
+            string lowercase = spaceless.ToLower();
+            // Convert sentence to a array
+            char[] reversed = lowercase.ToCharArray();
+            // Goes through the array from the front
+            for(int x = 0; x < reversed.Length; x++)
+            {
+                // Goes through the array from the back
+                for(int y = reversed.Length - 1; y >= 0; y--)
+                {
+                    // Compare letter by letter
+                    if (reversed[x] == reversed[y])
+                    {
+                        word = true;
+
+                    }
+                    else
+                    {
+                        word = false;
+                    }
+                }
+                
+                
+            }
+            return word;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Race car"));
+        }
+    }
+}
